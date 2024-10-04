4d5246ae172f06506b7191e72af90404b44a334b
amkhrjee <aniruddha.mukherji@outlook.com>
2024-10-04 18:11:30 +0530

    settings almost done

--- main.go
+++ main.go
@@ -30,8 +30,24 @@ func makeGrid() *grid.Grid {
 	return &g
 }
 
+const (
+	LIGHT int32 = 0
+	DARK  int32 = 1
+)
+
 func button(index int) rl.Rectangle {
 	return rl.Rectangle{X: float32(12*(index+1)) + float32(index*250), Y: 815, Width: 250, Height: 70}
+}
+
+func rectangle(x int, y int) rl.Rectangle {
+	xPadding := float32(10)
+	yOffset := float32(40)
+	return rl.Rectangle{
+		X:      200 + float32(x)*200 + xPadding,
+		Y:      200 + float32(y+1)*yOffset + 20,
+		Width:  150,
+		Height: 20,
+	}
 }
 
 func main() {
@@ -52,6 +68,8 @@ func main() {
 	var final_path []*grid.Box = nil
 	var visited []*grid.Box = nil
 
+	isSettingsOpen := false
+
 	timer := float32(0.)
 	trackIndex := 0
 
@@ -62,11 +80,35 @@ func main() {
 	curr_algo := algorithm.ASTAR
 	algo_name := "A* Search"
 
+	kValue := int32(2)
+	kvalueEditMode := false
+
+	theme := LIGHT
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
 
 		timer += rl.GetFrameTime()
+
+		// Draws the grid
+		for _, row := range g {
+			for _, box := range row {
+				r := rl.Rectangle{
+					X:      float32(box.Col*grid.BOX_DIM + grid.PADDING),
+					Y:      float32(box.Row*grid.BOX_DIM + grid.PADDING),
+					Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
+					Height: float32(grid.BOX_DIM) - 2*grid.PADDING}
+
+				if box.IsSource {
+					rl.DrawRectangleLinesEx(r, 10.0, rl.Blue)
+				}
+				if box.IsTarget {
+					rl.DrawRectangleLinesEx(r, 10.0, rl.Red)
+				}
+				rl.DrawRectangleRec(r, grid.Colors[box.Cost-1])
+			}
+		}
 
 		if rgui.Button(button(0), "#211# Reset") || rl.IsKeyPressed(rl.KeyR) {
 			if source != nil {
@@ -92,7 +134,9 @@ func main() {
 			}
 		}
 
-		rgui.Button(button(1), "#142# Settings")
+		if rgui.Button(button(1), "#142# Settings") {
+			isSettingsOpen = !isSettingsOpen
+		}
 
 		if rgui.Button(button(2), "#97# "+algo_name) {
 			curr_algo = (curr_algo + 1) % algorithm.ALGORITHMS_COUNT
@@ -113,132 +157,173 @@ func main() {
 			}
 		}
 
-		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
-			m := rl.GetMousePosition()
-			if m.Y < 800 {
-				selected := &g[int(m.Y/grid.BOX_DIM)][int(m.X/grid.BOX_DIM)]
-				// setting the source
-				if !target_set && !source_set {
-					selected.IsSource = true
-					source_set = true
-					source = selected
-				} else {
-					if source_set && !target_set && selected != source {
-						selected.IsTarget = true
-						target_set = true
-						target = selected
+		if !isSettingsOpen {
+
+			if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+				m := rl.GetMousePosition()
+				if m.Y < 800 {
+					selected := &g[int(m.Y/grid.BOX_DIM)][int(m.X/grid.BOX_DIM)]
+					// setting the source
+					if !target_set && !source_set {
+						selected.IsSource = true
+						source_set = true
+						source = selected
+					} else {
+						if source_set && !target_set && selected != source {
+							selected.IsTarget = true
+							target_set = true
+							target = selected
+						}
 					}
-				}
 
+				}
 			}
-		}
 
-		if source_set && target_set && track == nil {
-			switch curr_algo {
-			case algorithm.ASTAR:
-				track, final_path = algorithm.AStar(g, source, target)
-			case algorithm.UCS:
-				track, final_path = algorithm.Ucs(g, source, target)
-			case algorithm.BFS:
-				track, final_path = algorithm.Bfs(g, source, target)
-			case algorithm.DFS:
-				track, final_path = algorithm.Dfs(g, source, target)
-			case algorithm.LBEAM:
-				track, final_path = algorithm.LBeam(g, source, target, 2)
+			if source_set && target_set && track == nil {
+				switch curr_algo {
+				case algorithm.ASTAR:
+					track, final_path = algorithm.AStar(g, source, target)
+				case algorithm.UCS:
+					track, final_path = algorithm.Ucs(g, source, target)
+				case algorithm.BFS:
+					track, final_path = algorithm.Bfs(g, source, target)
+				case algorithm.DFS:
+					track, final_path = algorithm.Dfs(g, source, target)
+				case algorithm.LBEAM:
+					track, final_path = algorithm.LBeam(g, source, target, 2)
+				}
 			}
-		}
 
-		// Draws the grid
-		for _, row := range g {
-			for _, box := range row {
-				r := rl.Rectangle{
-					X:      float32(box.Col*grid.BOX_DIM + grid.PADDING),
-					Y:      float32(box.Row*grid.BOX_DIM + grid.PADDING),
-					Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
-					Height: float32(grid.BOX_DIM) - 2*grid.PADDING}
-
-				if box.IsSource {
-					rl.DrawRectangleLinesEx(r, 10.0, rl.Blue)
-				}
-				if box.IsTarget {
-					rl.DrawRectangleLinesEx(r, 10.0, rl.Red)
-				}
-				rl.DrawRectangleRec(r, grid.Colors[box.Cost-1])
-			}
-		}
-
-		if track != nil && trackIndex < len(track) {
-			if timer >= 0.05 {
-				// if timer >= 0.02 {
-				box := track[trackIndex]
-				if !slices.Contains(visited, box) {
-					visited = append(visited, box)
-				}
-				r := rl.Rectangle{
-					X:      float32(box.Col * grid.BOX_DIM),
-					Y:      float32(box.Row * grid.BOX_DIM),
-					Width:  float32(grid.BOX_DIM),
-					Height: float32(grid.BOX_DIM)}
-				rl.DrawRectangleRec(r, rl.Blue)
-				trackIndex++
-
-				timer = 0.0
-			}
-			for _, t := range track[:trackIndex-1] {
-				r := rl.Rectangle{
-					X:      float32(t.Col*grid.BOX_DIM + grid.PADDING),
-					Y:      float32(t.Row*grid.BOX_DIM + grid.PADDING),
-					Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
-					Height: float32(grid.BOX_DIM - 2*grid.PADDING)}
-				rl.DrawRectangleRec(r, rl.Pink)
-
-				if t == source {
-					rl.DrawRectangleRec(r, rl.Red)
-				}
-
-				if slices.Contains(final_path, t) {
-					x := float32(t.Col*grid.BOX_DIM + grid.PADDING)
-					y := float32(t.Row*grid.BOX_DIM + grid.PADDING)
+			if track != nil && trackIndex < len(track) {
+				if timer >= 0.05 {
+					// if timer >= 0.02 {
+					box := track[trackIndex]
+					if !slices.Contains(visited, box) {
+						visited = append(visited, box)
+					}
 					r := rl.Rectangle{
-						X:      x,
-						Y:      y,
+						X:      float32(box.Col * grid.BOX_DIM),
+						Y:      float32(box.Row * grid.BOX_DIM),
+						Width:  float32(grid.BOX_DIM),
+						Height: float32(grid.BOX_DIM)}
+					rl.DrawRectangleRec(r, rl.Blue)
+					trackIndex++
+
+					timer = 0.0
+				}
+				for _, t := range track[:trackIndex-1] {
+					r := rl.Rectangle{
+						X:      float32(t.Col*grid.BOX_DIM + grid.PADDING),
+						Y:      float32(t.Row*grid.BOX_DIM + grid.PADDING),
 						Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
 						Height: float32(grid.BOX_DIM - 2*grid.PADDING)}
-					rl.DrawRectangleRec(r, rl.Red)
+					rl.DrawRectangleRec(r, rl.Pink)
 
+					if t == source {
+						rl.DrawRectangleRec(r, rl.Red)
+					}
+
+					if slices.Contains(final_path, t) {
+						x := float32(t.Col*grid.BOX_DIM + grid.PADDING)
+						y := float32(t.Row*grid.BOX_DIM + grid.PADDING)
+						r := rl.Rectangle{
+							X:      x,
+							Y:      y,
+							Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
+							Height: float32(grid.BOX_DIM - 2*grid.PADDING)}
+						rl.DrawRectangleRec(r, rl.Red)
+
+					}
 				}
 			}
-		}
-		if track != nil && trackIndex >= len(track) {
+			if track != nil && trackIndex >= len(track) {
 
-			for _, f := range final_path {
+				for _, f := range final_path {
+					r := rl.Rectangle{
+						X:      float32(f.Col*grid.BOX_DIM + grid.PADDING),
+						Y:      float32(f.Row*grid.BOX_DIM + grid.PADDING),
+						Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
+						Height: float32(grid.BOX_DIM) - 2*grid.PADDING}
+					rl.DrawRectangleRec(r, rl.Red)
+				}
+
 				r := rl.Rectangle{
-					X:      float32(f.Col*grid.BOX_DIM + grid.PADDING),
-					Y:      float32(f.Row*grid.BOX_DIM + grid.PADDING),
+					X:      float32(track[len(track)-1].Col*grid.BOX_DIM + grid.PADDING),
+					Y:      float32(track[len(track)-1].Row*grid.BOX_DIM + grid.PADDING),
 					Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
-					Height: float32(grid.BOX_DIM) - 2*grid.PADDING}
-				rl.DrawRectangleRec(r, rl.Red)
+					Height: float32(grid.BOX_DIM - 2*grid.PADDING)}
+				rl.DrawRectangleRec(r, rl.Green)
 			}
 
-			r := rl.Rectangle{
-				X:      float32(track[len(track)-1].Col*grid.BOX_DIM + grid.PADDING),
-				Y:      float32(track[len(track)-1].Row*grid.BOX_DIM + grid.PADDING),
-				Width:  float32(grid.BOX_DIM - 2*grid.PADDING),
-				Height: float32(grid.BOX_DIM - 2*grid.PADDING)}
-			rl.DrawRectangleRec(r, rl.Green)
+			if track != nil {
+				l := rl.Rectangle{
+					X:      0,
+					Y:      750,
+					Width:  150,
+					Height: 50,
+				}
+				rl.DrawRectangleRec(l, color.RGBA{rl.RayWhite.R, rl.RayWhite.G, rl.RayWhite.B, 200})
+				rgui.Label(l,
+					fmt.Sprintf(" Visited: %d", len(visited)),
+				)
+			}
 		}
 
-		if track != nil {
-			l := rl.Rectangle{
-				X:      0,
-				Y:      750,
-				Width:  150,
-				Height: 50,
+		if isSettingsOpen {
+			r := rl.Rectangle{
+				X:      200,
+				Y:      200,
+				Width:  400,
+				Height: 400,
 			}
-			rl.DrawRectangleRec(l, color.RGBA{rl.RayWhite.R, rl.RayWhite.G, rl.RayWhite.B, 200})
-			rgui.Label(l,
-				fmt.Sprintf(" Visited: %d", len(visited)),
-			)
+			rl.DrawRectangleRec(r, rl.RayWhite)
+			rgui.WindowBox(r, "#142# Settings")
+
+			rgui.Label(rectangle(0, 0), "Local Beam K")
+			res := rgui.Spinner(rectangle(1, 0), "", &kValue, 1, 4, kvalueEditMode)
+			if res < 1 || res > 4 {
+				kvalueEditMode = !kvalueEditMode
+			}
+
+			// rgui.Label(rectangle(0, 1), "Theme")
+
+			rgui.Label(rectangle(0, 1), "Speedup (x)")
+			speed := rgui.Spinner(rectangle(1, 1), "", &kValue, 1, 4, kvalueEditMode)
+			if speed < 1 || speed > 4 {
+				kvalueEditMode = !kvalueEditMode
+			}
+
+			n := rectangle(0, 2)
+			theme = rgui.ToggleGroup(rl.Rectangle{
+				X:      n.X + 40,
+				Y:      n.Y,
+				Height: 40,
+				Width:  150,
+			}, "Light Mode;Dark Mode", theme)
+
+			beg := 4
+			f := rectangle(0, beg+1)
+			five := rectangle(0, beg+2)
+			s := rectangle(0, beg+3)
+			rgui.Label(rl.Rectangle{
+				X:      f.X + 110,
+				Y:      f.Y,
+				Width:  200,
+				Height: 20,
+			}, "Pathfinder v0.1.3")
+			rgui.Label(rl.Rectangle{
+				X:      five.X + 120,
+				Y:      five.Y,
+				Width:  200,
+				Height: 20,
+			}, "by @amkhrjee")
+			rgui.Label(rl.Rectangle{
+				X:      s.X + 60,
+				Y:      s.Y,
+				Width:  300,
+				Height: 20,
+			}, "made at Tezpur University")
+
 		}
 
 		rl.EndDrawing()
